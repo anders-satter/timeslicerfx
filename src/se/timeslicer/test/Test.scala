bb1604ef6b23b7398f2ad759354934eb49230c5f
Anders Sätter <anders.satter@telia.com>
2014-12-25 20:25:35 +0100

    added function to return a complete list of days in an interval, with day name, ex Fri

--- src/se/timeslicer/test/Test.scala
+++ src/se/timeslicer/test/Test.scala
@@ -7,45 +7,90 @@ import se.timeslicer.util.ItemUtil
 import se.timeslicer.reporting.Activity
 import se.timeslicer.reporting.Project
 import se.timeslicer.reporting.IntervalResult
+import scala.collection.mutable.ListBuffer
 
 /**
  * Test the different parts of the application
  */
 object Test {
   def main(args: Array[String]) {
-	 val i = Item("2014-11-23 09:00", 
-	     "2014-11-23 09:45",
-	     "Team TDE", 
-	     "scrum", 
-	     "it is ok...", 
-	     DateTime.getDayValueInMs("2014-11-23 09:00"))
+    val i = Item("2014-11-23 09:00",
+      "2014-11-23 09:45",
+      "Team TDE",
+      "scrum",
+      "it is ok...",
+      DateTime.getDayValueInMs("2014-11-23 09:00"))
   }
-  
+
   /*
    * Try and read from the logs 
    */
   val logLines = FileUtil.readFromFile("/Users/anders/dev/eclipse_ws1/TimeslicerFX/data/log.txt")
-   /*
+  /*
    * create a list of items
    */
-  
+
   val interval = new IntervalResult()
   interval.start = "2013-11-01"
-  interval.end = "2013-11-07" 
- 
-  interval.itemList = logLines.map(ItemUtil.parseLogItem).filter(_ != null).sortBy(_.dayValue)
-  interval.selection = ItemUtil.itemsInInterval(interval.itemList, interval.start, interval.end) 
-  
+  interval.end = "2013-11-07"
+
+  val itemList = logLines.map(ItemUtil.parseLogItem).filter(_ != null).sortBy(_.dayValue)
+
+  /*
+   *  convert the list to itemes
+   */
+
+  interval.itemList = itemList
+  interval.selection = ItemUtil.itemsInInterval(interval.itemList, interval.start, interval.end)
+
   /*
    * map.key = project, value = array of activities
    */
   val byProject = interval.selection.groupBy(_.project)
-  
+
   interval.projectList = byProject.map(entry => {
-	 new Project(entry._1, entry._2) 
+    new Project(entry._1, entry._2)
   })
-  
+
   interval.projectList.foreach(_.compile)
   interval.totalTime = interval.projectList.map(_.totalTime).reduceLeft(_ + _)
   interval.present
- }
+
+  /*
+   * SUMMARY OF EACH DAY
+   */
+  val sortedGroupByDayMap:Seq[(Long, Array[Item])] = interval.selection.groupBy(_.dayValue)
+  /**
+   * group by returns map, which is always unsorted, 
+   * so we need to get a sorted seq to get the days
+   * in the right order
+   */
+  .toSeq.sortBy(_._1)
+  
+  case class DaySum(dayValue: Long, day: String, sum: Double)
+
+  def createDaySum(dayValue: Long, dayItems: Array[Item]) = {
+    val sumOfDay = DateTime.getDecimalHours(dayItems.map(_.duration).reduceLeft(_ + _))
+    val dayStr = DateTime.getDayValueInStr(dayValue)
+    DaySum(dayValue, dayStr, sumOfDay)
+  }
+  val daySumList = sortedGroupByDayMap.map(i => createDaySum(i._1, i._2))
+
+  daySumList.map(println)
+
+  /*
+   * CALENDAR
+   */
+  
+  /*
+   * list of dates
+   */
+  
+  
+  
+  
+  
+  
+  
+  
+}
--- src/se/timeslicer/util/DateTime.scala
+++ src/se/timeslicer/util/DateTime.scala
@@ -1,23 +1,83 @@
 package se.timeslicer.util
 
 import scala.math.BigDecimal
+import java.util.Date
+import java.util.Locale
+
+
+case class CalendarDay(dayValue: Long, day: String, name: String)
 
 object DateTime {
   private val format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm")
   private val formatDay = new java.text.SimpleDateFormat("yyyy-MM-dd")
+  private val formatDayName = new java.text.SimpleDateFormat("EE", Locale.ENGLISH)
+  private val oneDayMs: Long = 1000 * 60 * 60 * 24
+
+  
+  
   
   def elapsedMinutes(start: String, end: String) = {
     val d1 = format.parse(start)
     val d2 = format.parse(end)
     (d2.getTime() - d1.getTime()) / 1000 / 60
   }
-  
-  def getDayValueInMs(day:String):Long = {
+
+  def getDayValueInMs(day: String): Long = {
     formatDay.parse(day).getTime()
   }
-  
-  def getDecimalHours(minutes:Long):Double = {
-    return BigDecimal(minutes/60.0).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+  def getDayValueInStr(day: Long): String = {
+    formatDay.format(new java.util.Date(day))
+  }
+
+  def getDecimalHours(minutes: Long): Double = {
+    return BigDecimal(minutes / 60.0).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
+
+  def getNumberOfDaysInInterval(start: Long, end: Long): Int = {
+    ((end - start) / oneDayMs).toInt
+  }
+
+  def incrementor(initVal: Int): () => Int = {
+    var startVal: Int = initVal
+    () => {
+      startVal = startVal + 1
+      startVal
+    }
+  }
+
+  def incrementor2(initVal: Int): Map[String, () => Int] = {
+    var startVal: Int = initVal
+     return Map(
+      ("next", () => {
+        startVal = startVal + 1
+        startVal
+      }),
+      ("reset", () => {
+        startVal = initVal
+        startVal
+      }))
+  }
+
+  def nextDay = (day: Long) => day + oneDayMs
+
+  def dayIncrementor(startDay:Long):() => Long = {
+    var currentDay = startDay - oneDayMs
+    return ()=> {
+      currentDay = currentDay + oneDayMs 
+      currentDay
+    }
   }
   
+  def calendarDayList(start:Long, end:Long):List[CalendarDay] = {
+    val dInc = dayIncrementor(start);
+    (0 to getNumberOfDaysInInterval(start, end)).map(
+        i=> dInc()).map(
+            x => CalendarDay(x, DateTime.getDayValueInStr(x),formatDayName.format(new Date(x)))).toList
+  }
+  
+  
+  def main(args: Array[String]) {
+    calendarDayList(1383260400000L, 1383260400000L+(oneDayMs*10)).map(println)
+  }
 }
